572553690a0d8d9099322ece2b9f1bdde047d09a
nickunger99 <nicolas.unger@yahoo.com.br>
2023-05-02 10:30:15 -0300

    Teste de velocidade na inserção de dados em HashSet e ArrayList

--- src/br/com/alura/TestaAlunos.java
+++ src/br/com/alura/TestaAlunos.java
@@ -1,5 +1,6 @@
 package br.com.alura;
 
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -24,14 +25,34 @@ public class TestaAlunos {
 
         System.out.println(alunos.size());
 
-        for (String aluno: alunos) {
+        for (String aluno : alunos) {
             System.out.println(aluno);
         }
 
         alunos.forEach(System.out::println);
-        
+
         System.out.println(alunos);
 
 
+        Collection<Integer> numeros = new HashSet<Integer>();
+        //Collection<Integer> numeros = new ArrayList<Integer>();
+
+        long inicio = System.currentTimeMillis();
+
+        for (int i = 1; i <= 50000; i++) {
+            numeros.add(i);
+        }
+
+        for (Integer numero : numeros) {
+            numeros.contains(numero);
+        }
+
+        long fim = System.currentTimeMillis();
+
+        long tempoDeExecucao = fim - inicio;
+
+        System.out.println("Tempo gasto: " + tempoDeExecucao);
+
     }
 }
+
