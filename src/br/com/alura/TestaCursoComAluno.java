e8df2d356d91e1f91210ab882f6b559223d77c19
nickunger99 <nicolas.unger@yahoo.com.br>
2023-05-02 14:15:17 -0300

    8 - Outros sets e iterators

--- src/br/com/alura/TestaCursoComAluno.java
+++ src/br/com/alura/TestaCursoComAluno.java
@@ -1,5 +1,8 @@
 package br.com.alura;
 
+import java.util.Iterator;
+import java.util.Set;
+
 public class TestaCursoComAluno {
     public static void main(String[] args) {
         Curso javaColecoes = new Curso("Dominando as coleções do Java", "Paulo Silveira");
@@ -18,9 +21,24 @@ public class TestaCursoComAluno {
         javaColecoes.matricula(a3);
 
         System.out.println("Todos os alunos matriculados: ");
-        javaColecoes.getAlunos().forEach(a -> {
-            System.out.println(a);
-        });
+
+        Set<Aluno> alunos = javaColecoes.getAlunos();
+        Iterator<Aluno> iterator = alunos.iterator();
+        while (iterator.hasNext()) {
+            Aluno proximo = iterator.next();
+            System.out.println(proximo);
+        }
+
+        //Vector<Aluno> vetor = new Vector<>();
+
+
+//        for (Aluno a : javaColecoes.getAlunos()) {
+//            System.out.println(a);
+//        }
+
+//        javaColecoes.getAlunos().forEach(a -> {
+//            System.out.println(a);
+//        });
 
         System.out.println("O " + a1 + " está matriculado?");
         System.out.println(javaColecoes.estaMatriculado(a1));
@@ -37,7 +55,6 @@ public class TestaCursoComAluno {
         // obrigatoriamente o seguinte é true:
 
         System.out.println(a1.hashCode() == turini.hashCode());
-
 
 
     }
