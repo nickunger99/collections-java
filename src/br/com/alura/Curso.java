7600bcebd441f57119101a5fdc953c1a50cf042f
nickunger99 <nicolas.unger@yahoo.com.br>
2023-05-02 09:41:09 -0300

    4- Mais Práticas com relacionamentos

--- src/br/com/alura/Curso.java
+++ src/br/com/alura/Curso.java
@@ -28,7 +28,23 @@ public class Curso {
         return Collections.unmodifiableList(aulas);
     }
 
-    public void adiciona(Aula aula){
+    public void adiciona(Aula aula) {
         this.aulas.add(aula);
+    }
+
+
+    public int getTempoTotal() {
+//        int tempoTotal = 0;
+//        for( Aula aula: aulas){
+//            tempoTotal += aula.getTempo();
+//        }
+//        return tempoTotal;
+        return this.aulas.stream().mapToInt(Aula::getTempo).sum();
+    }
+
+    @Override
+    public String toString() {
+        return "[Curso: " + nome + ", tempo total: " + this.getTempoTotal() + ","
+                + "aulas: " + this.aulas + "]";
     }
 }
