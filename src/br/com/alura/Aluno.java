93154e742bb8bb54d48acf3650b42399e3257b1e
nickunger99 <nicolas.unger@yahoo.com.br>
2023-05-02 13:50:47 -0300

    7 - Equals e HashCode

--- src/br/com/alura/Aluno.java
+++ src/br/com/alura/Aluno.java
@@ -1,10 +1,15 @@
 package br.com.alura;
 
+import java.util.Objects;
+
 public class Aluno {
     private String nome;
     private int numeroMatricula;
 
     public Aluno(String nome, int numeroMatricula) {
+        if (nome == null) {
+            throw new NullPointerException("nome não pode ser nulo");
+        }
         this.nome = nome;
         this.numeroMatricula = numeroMatricula;
     }
@@ -16,9 +21,33 @@ public class Aluno {
     public int getNumeroMatricula() {
         return numeroMatricula;
     }
+//    @Override
+//    public boolean equals(Object obj) {
+//        Aluno outro = (Aluno) obj;
+//        return this.nome.equals(outro.nome);
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        return this.nome.hashCode();
+//    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Aluno aluno = (Aluno) o;
+        return numeroMatricula == aluno.numeroMatricula && Objects.equals(nome, aluno.nome);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, numeroMatricula);
+    }
 
     @Override
     public String toString() {
         return "[Aluno: " + this.nome + ", matricula: " + this.numeroMatricula + "]";
     }
+
 }
